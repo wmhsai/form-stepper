1b8093dfb7d90d755eb7a0d9c7c18dc98312880f
wmhsai <mahsa.malmir769@gmail.com>
2024-07-31 17:53:51 +0330

    create enums

---
+++ src/utils/enums.ts
@@ -0,0 +1,22 @@
+
+export enum METHOD {
+    GET = 'get',
+    DELETE = 'delete',
+    POST = 'post',
+    PUT = 'put',
+    PATCH = 'patch'
+}
+
+export enum ALERT_TYPES {
+    SUCCESS = 'success',
+    ERROR = 'error',
+    WARNING = 'warning',
+    INFO = 'info'
+}
+
+export enum INPUT_TYPES {
+    NATIONAL_CODE = 'NationalCode',
+    PHONE_NUMBER = 'PhoneNumber',
+    BANK_ACCOUNT = "BankAccount",
+    SHABA_ACCOUNT = "ShabaAccount",
+}
