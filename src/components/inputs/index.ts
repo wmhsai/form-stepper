d0de97c47842d3d1304b3bf1fcca0a758caad2e1
wmhsai <mahsa.malmir769@gmail.com>
2024-07-31 23:02:02 +0330

    global style inputs

---
+++ src/components/inputs/index.ts
@@ -0,0 +1,32 @@
+export const styles = {
+    textInputWrapper: {
+        width: '20rem',
+        display: "flex",
+        flexDirection: "column",
+    },
+    spanStyle: {
+        color: 'red',
+        fontSize: "8pt"
+    },
+    inputStyle: {
+        height: "2rem",
+        borderRadius: "1rem",
+        padding: ".2rem",
+    },
+    labelStyle: {
+        fontSize: "10pt",
+        paddingBottom: ".2rem"
+    },
+    selectStyle: {
+        width: '20rem',
+        height: "2rem",
+        fontSize: "12pt",
+        borderRadius: "1rem",
+        padding: ".2rem",
+      },
+      comboWrapper: {
+        display: "flex",
+        flexDirection: "column",
+        gap: ".1rem"
+      }
+}
