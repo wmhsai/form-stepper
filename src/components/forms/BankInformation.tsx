618253d53e59563c42e6777ed5243bd03aa80ae0
wmhsai <mahsa.malmir769@gmail.com>
2024-07-31 17:51:43 +0330

    use type for validation in  BankInformation

--- src/components/forms/BankInformation.tsx
+++ src/components/forms/BankInformation.tsx
@@ -1,15 +1,37 @@
 import { useFormContext } from "react-hook-form";
 import { Fragment } from "react/jsx-runtime";
 import NumberInput from "../inputs/NumberInput";
+import { INPUT_TYPES } from "../../utils/enums";
+import TextInput from "../inputs/TextInput";
+import SeparatedPriceInput from "../inputs/SeparatedPriceInput";
 
 const BankInformation = () => {
   const { control } = useFormContext();
   return (
     <Fragment>
       <div style={{ ...styles.FormWrapper as React.CSSProperties }}>
-        <NumberInput control={control} name="AccountNumber" label="َشماره حساب" required />
-        <NumberInput control={control} name="ShabaNumber" label="شماره شبا" required/>
-        <NumberInput control={control} name="AverageRiyal" label="میانگین ریالی موجودی سالیانه" required/>
+        <NumberInput 
+        control={control}
+         name="AccountNumber" 
+         label="شماره حساب" 
+         required 
+         type={INPUT_TYPES.BANK_ACCOUNT}
+         />
+        <TextInput 
+        control={control} 
+        name="ShabaNumber"
+         label="شماره شبا"
+         type={INPUT_TYPES.SHABA_ACCOUNT}
+         placeholder={"... IR123"}
+
+          required
+          />
+        <SeparatedPriceInput 
+        control={control}
+         name="AverageRiyal"
+          label="میانگین ریالی موجودی سالیانه"
+           required
+           />
       </div>
     </Fragment>)
 }
