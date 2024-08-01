f30874b5257f0bf08ed3fa7ad9e833ef1134843d
wmhsai <mahsa.malmir769@gmail.com>
2024-08-01 09:50:16 +0330

    create UserData type

---
+++ src/types/UserTypes.ts
@@ -0,0 +1,13 @@
+export interface UserData {
+    FirstName: string;
+    LastName: string;
+    AccountNumber: string;
+    AverageRiyal: string;
+    BirthDate: Date;
+    NationalCode: string;
+    PaidPeriod: string;
+    PhoneNumber: string;
+    ShabaNumber: string;
+    facilities: string;
+
+}
